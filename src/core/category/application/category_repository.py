2f5b47338b5a79b733e1a01aa671cd02bc8bbbbc
Fabio Faria <fabiofa87@gmail.com>
2024-03-04 18:40:45 -0300

    Add type hinting for id parameter in get_by_id method

--- src/core/category/application/category_repository.py
+++ src/core/category/application/category_repository.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
+
+from src.core.category.domain.category import Category
 
 
 class CategoryRepository(ABC):
@@ -7,5 +10,5 @@ class CategoryRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get_by_id(self, id):
+    def get_by_id(self, id: UUID) -> Category | None:
         raise NotImplementedError
