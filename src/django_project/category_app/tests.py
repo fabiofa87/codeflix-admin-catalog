c95cabb112a37468b65b5a999071fc22c53f6b8e
Fabio Faria <fabiofa87@gmail.com>
2024-03-05 20:23:33 -0300

    Add test for listing categories using the API

--- src/django_project/category_app/tests.py
+++ src/django_project/category_app/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestCategoryAPI(APITestCase):
+    def test_list_categories(self):
+        response = self.client.get("/api/categories/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello, world!"})
