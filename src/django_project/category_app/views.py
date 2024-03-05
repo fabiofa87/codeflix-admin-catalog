ae9014f5e1e23370c66fb1beeab1e9ae2764122d
Fabio Faria <fabiofa87@gmail.com>
2024-03-05 20:10:33 -0300

    Add CategoryViewSet and rest_framework to INSTALLED_APPS

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+    def list(self, request: Request) -> Response:
+        return Response(status=HTTP_200_OK, data={"message": "Hello, world!"})
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
